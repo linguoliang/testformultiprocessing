7fae06cf4efeb612a8e9f113251e21681ba698f9
linguoliang <linguoliang1313@gmail.com>
2016-09-28 23:05:38 +0800

    update files

--- plotslidingwindow.py
+++ plotslidingwindow.py
@@ -1,12 +1,14 @@
 #codig=UTF-8
 import seaborn as sb
-from matplotlib import pylab as pl
+from matplotlib import pylab
 from matplotlib import pyplot as plt
+import matplotlib
 import numpy
 import optparse
 import sys
 import time
 import GTF_decoding
+# from sliding_windows import sliding_window
 
 _author__ = 'Guoliang Lin'
 Softwarename = 'plotslidingwindow'
@@ -47,22 +49,61 @@ def _parse_args():
     #    parser.add_option('-f','--fpkm',dest='fpkm_file',type='string',help='input fpkm file')
     #    parser.add_option('-v','--variation', dest='variation', type='string', help='input variation information file')
     parser.add_option('-g', '--gtf', dest='gtf', help='gtf file')
-    parser.add_option('-o', '--output', dest='output', type='string', help='input variation information file')
-    parser.add_option('-d','--depth',dest='depth',type='int',default=5,help='depth')
+    parser.add_option('-o', '--output', dest='output', type='string', help='output prefix')
+    parser.add_option('-w','--windows-size',dest='window_size',type='int',default=3000000,help='window size')
     options, args = parser.parse_args()
     # positional arguments are ignored
     return options
 
 
+def slidingwindow(filehindle,schaffold,window_size):
+    filepointer = 0
+    last=[]
+    data=[]
+    end=window_size
+    while True:
+        if len(end)!=0:
+            data.append(last)
+            last=[]
+        elment=filehindle.readline()
+        if not elment:
+            yield data
+            raise StopIteration()
+        # filepointer=filehindle.tell()
+        elment=elment.strip()
+        tmp=elment.split('\t')
+        if tmp[0]!=schaffold:
+            yield data
+            data=[]
+            filehindle.seek(filepointer)
+            # raise StopIteration()
+        else:
+            data.append(tmp[1:3])
+        filepointer=filehindle.tell()
+        # end+=window_size
+        if int(data[-1][0])>window_size:
+            last=data.pop()
+            yield data
+            data=[]
+            end+=window_size
 
 
-
-
+def plotfig(windowdata):
+    data=numpy.array(windowdata)
+    fig=plt.bar(data[:,0],data[:,1])
+    fig[0].set_color("r")
+    plt.show()
 
 
 
 def mian():
     options=_parse_args()
+    GTF_decoding.decodegff(options.gtf)
+    with open(options.input) as filehindle:
+        for key in GTF_decoding.genomeDict.keys():
+            itrator=slidingwindow(filehindle,key,options.window_size)
+            for windowdata in itrator:
+                plotfig(windowdata)
 
 
 
--- test.py
+++ test.py
@@ -4,39 +4,63 @@
 from multiprocessing.dummy import Pool as Pl
 from multiprocessing import Array
 import time
-
+import numpy
 global list1
-list1 = []
+import plotslidingwindow
 
+size=[[1,5],[2,5],[3,6],[4,7],[5,7],[6,8],[7,8],[8,9],[9,9],[10,9],[11,7],[12,7],[13,6],[14,6],[17,6],[18,3],[19,7]]
 
-def fileinputiterator(filehandle):
-    multinum = []
-    conter = 0
-    for elment in filehandle:
-        elment.strip()
-        # conter+=1
-        # multinum.append(elment)
-        # if conter==10:
-        #     yield multinum
-        #     conter=0
-        #     multinum=[]
-        yield elment
-
-
-def printnumbers(element):
-    global list1
-    list1.append(element)
-    pass
-
-
-if __name__ == "__main__":
-    t1 = time.time()
-    with Pl(1) as pool:
-        with open("testdata.txt") as ipf:
-            # file=fileinputiterator(ipf)
-            pool.map(printnumbers, ipf)
-            # for element in ipf:
-            #     printnumbers(element)
-    t2 = time.time()
-    print(t2 - t1)
-    print(len(list1))
+plotslidingwindow.plotfig(size)
+# list1 = [[1,2],[3,4],[4,6]]
+# # list2=list1[:,0]
+# b=numpy.array(list1)
+# print(b[:,0])
+# tmp = []
+# if len(tmp) == 0:
+#     print('haha')
+#
+#
+# # x=tmp.split('\t')
+#
+# def fileinputiterator(filehandle):
+#     multinum = []
+#     conter = 0
+#     flag = True
+#     while True:
+#         # for elment in filehandle:
+#         elment = filehandle.readline()
+#         if not elment:
+#             raise StopIteration()
+#         elment = elment.strip()
+#         x = filehandle.tell()
+#         print(conter)
+#         conter += 1
+#         # multinum.append(elment)
+#         # if conter==10:
+#         #     yield multinum
+#         #     conter=0
+#         #     multinum=[]
+#         yield elment
+#         if conter >= 20000:
+#             raise StopIteration()
+#
+#
+# def printnumbers(element):
+#     global list1
+#     list1.append(element)
+#     print(element)
+#     pass
+#
+#
+# if __name__ == "__main__":
+#     t1 = time.time()
+#     with Pl(1) as pool:
+#         with open("testdata.txt") as ipf:
+#             file = fileinputiterator(ipf)
+#             # x=pool.map(printnumbers, file)
+#             for element in file:
+#                 x = ipf.tell()
+#                 # printnumbers(x)
+#     t2 = time.time()
+#     print(t2 - t1)
+#     print(len(list1))
